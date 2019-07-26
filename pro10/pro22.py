3339106dac2b2fc38f014f5e52fed84e3b45b830
Rihanashariff <52313932+Rihanashariff@users.noreply.github.com>
2019-07-26 10:29:09 -0700

    Update pro22.py

--- pro10/pro22.py
+++ pro10/pro22.py
@@ -1,7 +1,7 @@
 p=int(input())
-pu=list(map(int,input().split()))[:y]
-ri=sum(pu[0:y:2])
-ak=sum(pu[1:y:2])
+pu=list(map(int,input().split()))[:p]
+ri=sum(pu[0:p:2])
+ak=sum(pu[1:p:2])
 if ri>ak:
   print(ri)
 else:
