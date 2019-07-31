e78c1dae22ad94e6c74e7ffbb2bb24b8134a58a6
Rihanashariff <52313932+Rihanashariff@users.noreply.github.com>
2019-07-31 15:49:09 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,43 @@
+userid=input("enter the id::")
+pin=input("enter the 4 digit pin::")
+data=["santhiya","4321"]
+if(userid and pin in data):
+  print("valid")
+else:
+  print("invalid")
+  exit;
+  def valid():
+    if(valid): 
+        def balance():
+          amt=int(input("entire amount::8000"))
+          print((balance)
+        def withdrawl():
+          amt=int(input("what amount of withdrawl:2000"))
+          print("new balance",balance-amt)
+        def deposit():
+        amt=int(input("what amount of deposit:3000"))
+        print("new balance",balance+amt)
+        choice=int(input("choose an option:1.balance 2.withdrawl 3.deposit"))
+        if choice==1:
+          print(balance);
+          elif choice==2:
+            print(withdrawl);
+            else choice==3:
+              print(deposit);
+              def current():
+                amt=int(input("recently add amount:0"))
+                print("new balance",balance+amt)
+                def savings():
+                  amt=int(input("already saved amount:8000"))
+                  print("new balance")
+                  choice=int(input("choose an option:1.current 2.savings"))
+                  if choice==1:
+                    print(current);
+                    elif choice==2:
+                      print(savings);
+                      else:
+                        print(exit);
+                       
+       print("withdrawl:2000")
+       print("transaction has been successful")
+        
